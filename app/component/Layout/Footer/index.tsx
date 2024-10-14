7136a8b93081ac00117de84d4080556f55f8a970
quyetdaik <bquyet09@gamil.com>
2024-10-14 21:02:57 +0700

    [dev] add data list tech of recent project and email contact

--- app/component/Layout/Footer/index.tsx
+++ app/component/Layout/Footer/index.tsx
@@ -6,21 +6,19 @@ import { FaLocationArrow } from 'react-icons/fa6'
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
-      {/* background grid */}
-      {/* <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div> */}
-
       <div className="flex flex-col items-center">
-        {/* <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you achieve your goals.
-        </p> */}
-        <a href="mailto:bquyet09@gmail.com">
-          <MagicButton title="Send me an email" icon={<FaLocationArrow />} position="right" />
+        <a>
+          <MagicButton
+            title="Send me an email"
+            icon={<FaLocationArrow />}
+            position="right"
+            handleClick={() => {
+              const email = 'bquyet09@gmail.com'
+              const gmailURL = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}`
+
+              window.open(gmailURL, '_blank')
+            }}
+          />
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
