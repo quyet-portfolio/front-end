817a5d53c4c23d38f5f6bbd4cf463c9eadf17a48
quyetdaik <bquyet09@gamil.com>
2024-10-19 22:46:01 +0700

    [dev] dev api and ui register page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
-"use client";
-import AboutMe from "./component/AboutMe";
-import Hero from "./component/Hero";
-import Navbar from "./component/Layout/Navbar";
-import { navItems } from "@/data/helper";
-import RecentProjects from "./component/RecentProjects";
-import WorkExperience from "./component/WorkExperience";
-import Footer from "./component/Layout/Footer";
+'use client'
+import AboutMe from './pages/component/AboutMe'
+import Hero from './pages/component/Hero'
+import Navbar from './pages/component/Layout/Navbar'
+import { navItems } from '@/data/helper'
+import RecentProjects from './pages/component/RecentProjects'
+import WorkExperience from './pages/component/WorkExperience'
+import Footer from './pages/component/Layout/Footer'
+import RegisterPage from './pages/register/route'
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <Navbar navItems={navItems}/>
+        {/* <RegisterPage /> */}
+        <Navbar navItems={navItems} />
         <Hero />
         <AboutMe />
         <RecentProjects />
@@ -23,5 +25,5 @@ export default function Home() {
         <Footer />
       </div>
     </main>
-  );
+  )
 }
