e1fd47b03482b9dae9fe177c00f268d16b23de2a
quyetdaik <bquyet09@gamil.com>
2024-10-19 23:10:18 +0700

    [fix] fix name router register

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* <RegisterPage /> */}
         <Navbar navItems={navItems} />
         <Hero />
         <AboutMe />
